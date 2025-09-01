bbc6fa6a45495db0e61da87593be0bbf16b99ab8
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-01 10:28:47 +0530

    Update 1.4.py

--- Exp_1/1.4.py
+++ Exp_1/1.4.py
@@ -1,9 +1,11 @@
 # Exp 1.4
 # Domain: Space
-# Einstein’s Equation: E = mc^2
-
-daivik_mass = 100  # kg
-daivik_c = 3 * (10**8)  # speed of light
-
+# Einstein’s Equation: E = mc**2 
+#In Python (**  = sqr == ^)
+# mass in kg
+daivik_mass = 100 
+# speed of light (c)
+daivik_c = 3 * (10**8)  
+# Conversion {e = mc**2}
 daivik_e = daivik_mass * (daivik_c**2)
 print(f'Energy (in J) = {daivik_e}')
