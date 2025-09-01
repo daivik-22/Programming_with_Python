5308dd40abc40a10d5a5a895c167ec743a7d5eec
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-01 10:26:34 +0530

    Update 1.3.py

--- Exp_1/1.3.py
+++ Exp_1/1.3.py
@@ -3,17 +3,15 @@ from math import *
 
 daivi_dc = 0.2
 daivik_ad = 1.2
-
 a_daivi = pi * 0.11**2
-sc_daivi = 1000.0 / 3600  # conversion factor
-
+# conversion factor
+sc_daivi = 1000.0 / 3600  
 v_daivik = 120 * sc_daivi
 u_daivik = 10 * sc_daivi
 
 # Hard kick
 daivik_f = 0.5 * daivi_dc * daivik_ad * a_daivi * v_daivik**2
 print('Hard kick:', daivik_f)
-
 # Soft kick
 daivik_f1 = 0.5 * daivi_dc * daivik_ad * a_daivi * u_daivik**2
 print('Soft kick:', daivik_f1)
