d0b970965b7181828166466f7a2b4fa1f0587103
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-01 10:22:42 +0530

    Create 1.4.py

---
+++ Exp_1/1.4.py
@@ -0,0 +1,9 @@
+# Exp 1.4
+# Domain: Space
+# Einstein’s Equation: E = mc^2
+
+daivik_mass = 100  # kg
+daivik_c = 3 * (10**8)  # speed of light
+
+daivik_e = daivik_mass * (daivik_c**2)
+print(f'Energy (in J) = {daivik_e}')
