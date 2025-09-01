fa83add65059deb15b1bbab5fbb9b8265d642dd7
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-01 10:25:36 +0530

    Update 1.2.py

--- Exp_1/1.2.py
+++ Exp_1/1.2.py
@@ -1,12 +1,12 @@
 # Exp 1.2
 from math import *
-
+#input 
 daivik_x = 0.5
 daivik_g = 9.81
 daivik_u = 10
 daivik_v0 = 10 / 3.6
-daivik_rad = (pi / 180) * 45  # converting degrees to radians
-
+# converting degrees to radians
+daivik_rad = (pi / 180) * 45 
 daivik_y = (daivik_x * tan(daivik_rad)) - (daivik_g * (daivik_x**2)) / ((2 * (daivik_v0**2)) * (cos(daivik_rad)**2))
-
+# Output 
 print("Projectile Y:", daivik_y)
