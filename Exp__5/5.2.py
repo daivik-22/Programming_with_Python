10b4eaf0b219f53214b684fc824dcdd5abedbe61
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-03 11:07:54 +0530

    Update 5.2.py

--- Exp__5/5.2.py
+++ Exp__5/5.2.py
@@ -1,17 +1,15 @@
+# K.Daivik Reddy 
+# RA2211043010021
+# Exp. 5.2 – Dataset Handling with Pandas
 import pandas as pd
-
 # Load dataset (you can replace with your own CSV file)
 df = pd.read_csv("sample_dataset.csv")
-
 # Display first 5 rows
 print("Head of dataset:\n", df.head())
-
 # Display last 5 rows
 print("\nTail of dataset:\n", df.tail())
-
 # Summary statistics
 print("\nSummary Statistics:\n", df.describe())
-
 # Info of dataset
 print("\nDataset Info:")
 print(df.info())
