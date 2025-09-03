ec8f0a3c77a85e407441d2beb6a1fb5f860eba6f
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-03 11:06:36 +0530

    Update 5.1.py

--- Exp__5/5.1.py
+++ Exp__5/5.1.py
@@ -1,17 +1,16 @@
+# K.Daivik Reddy
+# RA2211043010021
+# Exp. 5.1 – Create an Excel Sheet using Pandas
 import pandas as pd
-
 # Sample data
 data = {
     "Name": ["Alice", "Bob", "Charlie", "David"],
     "Age": [23, 25, 22, 24],
     "Marks": [88, 92, 79, 85]
 }
-
 # Create DataFrame
 df = pd.DataFrame(data)
-
 # Save to Excel
 df.to_excel("students.xlsx", index=False)
-
 print("Excel file 'students.xlsx' created successfully!")
 print(df)
