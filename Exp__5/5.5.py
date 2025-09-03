2b623f3e4197f6d21ef6e2fb157a404681866157
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-03 11:11:30 +0530

    Update 5.5.py

--- Exp__5/5.5.py
+++ Exp__5/5.5.py
@@ -1,5 +1,7 @@
+# K.Daivik Reddy
+# RA2211043010021
+# Exp. 5.5 – Hospital Patient Records
 import pandas as pd
-
 # Patient data
 data = {
     "PatientID": [101, 102, 103, 104],
@@ -7,14 +9,10 @@ data = {
     "Age": [45, 62, 70, 30],
     "BillAmount": [2000, 3500, 5000, 1500]
 }
-
 df = pd.DataFrame(data)
-
 # Apply 10% discount for senior citizens (Age > 60)
 df["Discount"] = df.apply(lambda row: row["BillAmount"] * 0.1 if row["Age"] > 60 else 0, axis=1)
 df["FinalAmount"] = df["BillAmount"] - df["Discount"]
-
 # Save to Excel
 df.to_excel("hospital_records.xlsx", index=False)
-
 print("Hospital Records:\n", df)
