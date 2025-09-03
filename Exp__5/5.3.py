ff0893da7eb9756ec475edb4b4ac53f50e3cd9b3
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-03 11:09:04 +0530

    Update 5.3.py

--- Exp__5/5.3.py
+++ Exp__5/5.3.py
@@ -1,5 +1,7 @@
+# K.Daivik Reddy
+# RA2211043010021
+# Exp. 5.3 – Weather Data Analysis using NumPy
 import numpy as np
-
 # Example: Assume weather.txt contains daily temperatures
 # Example content of weather.txt:
 # 30
@@ -7,12 +9,9 @@ import numpy as np
 # 29
 # 35
 # 31
-
 # Load data
 data = np.loadtxt("weather.txt")
-
 print("Weather Data:", data)
-
 # Basic analysis
 print("Max Temperature:", np.max(data))
 print("Min Temperature:", np.min(data))
