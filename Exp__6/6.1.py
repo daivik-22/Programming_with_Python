90992fca04d42ca7e6023faddd9c0a328196eae4
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-09-04 13:30:12 +0530

    Create 6.1.py

---
+++ Exp__6/6.1.py
@@ -0,0 +1,11 @@
+import sys, os
+os.getcwd()
+#os.chdir(r"D:\2025-2026\ODD\Python Programming\Notes\Codes")
+os.getcwd()
+
+file = open('spam.txt,'w')
+file.write(('spam'*5+'\n')
+file.close()
+file.open('spam.txt')
+text = file.read()
+text
