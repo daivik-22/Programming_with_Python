30c4b87a2a38709d9d0ea3a877d0600b890b08af
Daivik Reddy <155605327+daivik-22@users.noreply.github.com>
2025-10-15 20:23:20 +0530

    Create 10.5.py

---
+++ Exp_10/10.5.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+sites = [
+    "https://www.facebook.com",
+    "https://www.instagram.com",
+    "https://www.twitter.com",
+    "https://www.linkedin.com"
+]
+
+for site in sites:
+    webbrowser.open(site)
